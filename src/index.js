3bd97c378323c469a01b3ba09755f268089daada
tyler <tylercharawell@gmail.com>
2021-05-05 20:25:50 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newDescr = document.getElementById('new-task-description');
+  const submit = newDescr.nextElementSibling;
+  const ul = document.getElementById('tasks');
+  
+
+submit.addEventListener('click', createNewTask) 
+  
+function createNewTask(e){
+  const li = document.createElement('li')
+  const item = newDescr.value;
+  li.innerHTML = item;
+  ul.appendChild(li);
+  e.preventDefault();
+  }
 });
